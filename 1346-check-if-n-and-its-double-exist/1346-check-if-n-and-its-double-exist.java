97dd77b5136a435b95c27bfe93f96b8a5383e7e6
Tanmayee Chakraborty <162902253+tanmayee78@users.noreply.github.com>
2025-06-19 01:16:05 +0530

    Time: 4 ms (12.21%), Space: 42.9 MB (62.59%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,11 +1,28 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
+        //Brute Force
+        // for(int i = 0; i < arr.length; i++){
+        //     for(int j = 0; j < arr.length; j++){
+        //             if(i != j && arr[i] == 2 * arr[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        //Binary Search
+        Arrays.sort(arr);
         for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr.length; j++){
-                if(i != j){
-                    if(arr[i] == 2 * arr[j]){
+            int target = 2 * arr[i];
+            int start = 0, end = arr.length - 1;
+            while(start <= end){
+                int mid = start + (end - start) / 2;
+                if(arr[mid] == target && mid != i){
                     return true;
-                }
+                }else if(arr[mid] > target){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
                 }
             }
         }
